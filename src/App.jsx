40a37cdf431ec6f0e54005cb371094dd5855fd5c
Ahmad Hassan <ahassandevx@gmail.com>
2025-11-05 03:13:16 +0500

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <div>
-      Hello World
+      <Navbar />
+      <h1 className='text-white bg-red-500'>This is the 1st Heading</h1>
+
+      <div className='w-20 h-20 bg-amber-300 text-center flex items-center justify-center mt-2.5'>box</div>
     </div>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+        <p>this is a para</p>
+    </div>
+  )
+}
+
+export default Navbar
