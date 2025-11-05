063285400bd4ed24756683f0f14bd0de1dcba3a1
Ahmad Hassan <ahassandevx@gmail.com>
2025-11-05 23:04:51 +0500

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import React from 'react'
-import Navbar from './components/Navbar'
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
     <div>
-      <Navbar />
-      <h1 className='text-white bg-red-500'>This is the 1st Heading</h1>
-
-      <div className='w-20 h-20 bg-amber-300 text-center flex items-center justify-center mt-2.5'>box</div>
+      <Navbar/>
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
-
+import myimage from '../assets/Images/myimage.png';
 function Navbar() {
   return (
-    <div>
-        <p>this is a para</p>
-    </div>
+    <nav className='flex justify-between items-center mx-7'>
+      <div className='flex items-center gap-3'>
+      <img src={myimage} className='w-17 h-17 rounded-full' alt=""/>
+      <h1 className='text-xl'>Ahmad Hassan</h1>
+      </div>
+
+      <ul className='flex gap-3'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Features</a></li>
+        <li><a href="#">Portfolio</a></li>
+        <li><a href="#">Testimonial</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </nav>
   )
 }
 
