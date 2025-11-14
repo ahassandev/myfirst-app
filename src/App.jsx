1b9e29cd488c192f54a4f377b38c46c297844cd4
Ahmad Hassan <ahassandevx@gmail.com>
2025-11-14 21:52:58 +0500

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import Navbar from "./components/Navbar";
-import HeroSection from "./components/HeroSection";
-import Features from "./components/FeaturesSection";
-import Portfolio from "./components/PortfolioSection";
-import Skills from "./components/Skills";
-import Testimonial from "./components/TestimonialSection";
-import Blog from "./components/BlogSection";
-import Contact from "./components/ContactSection";
-import Footer from "./components/Footer";
+import Home from "./components/Pages/Home";
+// import Features from "../FeaturesSection";
+// import Portfolio from "../PortfolioSection";
+// import Skills from "../Skills";
+// import Testimonial from "../TestimonialSection";
+// import Blog from "../BlogSection";
+// import Contact from "../ContactSection";
+ import Footer from "./components/Footer";
 import React, { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import { Routes, Route } from "react-router-dom";
-import MainPage from "./components/MainPage";
+// import MainPage from "../MainPage";
 
 
 function App() {
@@ -29,12 +29,12 @@ function App() {
     <div>
       <Navbar />
       <Routes>
-        <Route path = "/" element = {<MainPage/>} />
-        <Route path = "/Features" element = {<Features/>} />
+        <Route path = "/" element = {<Home/>} />
+        {/* <Route path = "/Features" element = {<Features/>} />
         <Route path = "/Portfolio" element = {<Portfolio/>} />
         <Route path = "/Testimonial" element = {<Testimonial/>} />
         <Route path = "/Blog" element = {<Blog/>} />
-        <Route path = "/Contact" element = {<Contact/>} />
+        <Route path = "/Contact" element = {<Contact/>} /> */}
       </Routes>
       <Footer />
     </div>
---
+++ src/components/Pages/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import HeroSection from '../Home/HeroSection'
+import FeaturesSection from '../Home/FeaturesSection'
+import PortfolioSection from '../Home/PortfolioSection'
+import TestimonialSection from '../Home/TestimonialSection'
+import BlogSection from '../Home/BlogSection'
+import ContactSection from '../Home/ContactSection'
+
+function Home() {
+  return (
+    <>
+      <HeroSection/>
+      <FeaturesSection/>
+      <PortfolioSection/>
+      <TestimonialSection/>
+      <BlogSection/>
+      <ContactSection/>
+    </>
+  )
+}
+
+export default Home
